b63117768cb62ae9ef81f174345afa41d5a7a17f
sophia800 <sofiavasulyuk@gmail.com>
2021-04-05 22:35:47 +0300

    Some message

--- Src/BookStore.Domain/Models/Reader.cs
+++ Src/BookStore.Domain/Models/Reader.cs
@@ -2,6 +2,55 @@ namespace BookStore.Domain.Models
 {
     public class Reader
     {
-        
+        private string _name;
+        private string _email;
+        private string _city;
+        private string _street;
+        private string _house;
+        public void SetName ( string name )
+        {
+            _name = name;
+        }
+
+        public string GetName ()
+        {
+            return _name;
+        }
+        public void SetEmail ( string email )
+        {
+            _email = email;
+        }
+
+        public string GetEmail ()
+        {
+            return _email;
+        }
+        public void SetCity ( string city )
+        {
+            _city = city;
+        }
+
+        public string GetCity ()
+        {
+            return _city;
+        }
+        public void SetStreet ( string street )
+        {
+            _street = street;
+        }
+
+        public string GetStreet ()
+        {
+            return _street;
+        }
+        public void SetHouse ( string house )
+        {
+            _house = house;
+        }
+
+        public string GetHouse ()
+        {
+            return _house;
+        }
     }
 }
