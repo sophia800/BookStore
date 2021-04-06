f265519574b995d74197867a98d503785fd8b571
sophia800 <sofiavasulyuk@gmail.com>
2021-04-06 23:14:07 +0300

    Add new models & fix conflicts

--- Src/BookStore.Presentation/Console/Program.cs
+++ Src/BookStore.Presentation/Console/Program.cs
@@ -6,39 +6,39 @@
 	{
 		public static void Main ()
 		{
-			
-            Author author = new Author ();
+
+			Author author = new Author ();
 
 			author.SetName ( "some name" );
 
 			author.SetBiography ( "some bio" );
-            
-      
-            Book book = new Book ();
+
+
+			Book book = new Book ();
 
 			book.SetAuthor ( "some name" , "some bio" );
 
 
 			Reader reader = new Reader ();
 
-            reader.SetName ( "some name" );
+			reader.SetName ( "some name" );
 
-            reader.SetEmail ( "some email" );
+			reader.SetEmail ( "some email" );
 
-            reader.SetCity ( "some city" );
+			reader.SetCity ( "some city" );
 
-            reader.SetStreet ( "some street" );
-            
-            reader.SetHouse ( "some house" );
+			reader.SetStreet ( "some street" );
+
+			reader.SetHouse ( "some house" );
 
 
-            Order order = new Order ();
+			Order order = new Order ();
 
-            order.SetBook ( "some book" );
+			order.SetBook ( "some book" );
 
-            order.SetReader ( "some reader" );
-            
-            order.SetDate ( "some date" );
+			order.SetReader ( "some name" , "some email" , "some city" , "some street" , "some house" );
+
+			order.SetDate ( "some date" );
 
 
 			System.Console.WriteLine ( "All work" );
